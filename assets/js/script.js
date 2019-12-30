c9c45dab036d94e29757f5301859fe5b889b1b74
alexcct <ivanalejandro@MacBook-Air-de-Ivan.local>
2019-12-29 21:20:22 -0300

    second commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+/*SMOOTH SCROLL*/
+
+$(function(){
+	$("a").click(function(event){
+		if (this.hash !== "") {
+			event.preventDefault();
+			
+			var gato = this.hash;
+			
+			$("html, body").animate({ scrollTop: $(gato).offset() .top}, 800, function (){
+				window.location.hash = gato;
+				
+			});
+		}
+	});
+});
+
+
+
+
+/*TOOLTIP*/
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
